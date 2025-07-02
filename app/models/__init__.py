53144e7452d2610798ce624a4f7650fbfa97fe3e
MuhammadAman02 <mohammada@hst.ie>
2025-07-02 12:15:20 +0500

    Codegen: app/models/__init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,16 @@
-# This file makes this directory a Python package.
-# It's intended for data models & schemas (e.g., Pydantic, SQLAlchemy).
+"""Data models for the LinkedIn clone application."""
+
+from app.models.user import User
+from app.models.profile import Experience, Education, Skill
+from app.models.social import Connection, Post, Comment, Like
+
+__all__ = [
+    "User",
+    "Experience", 
+    "Education",
+    "Skill",
+    "Connection",
+    "Post",
+    "Comment", 
+    "Like"
+]
